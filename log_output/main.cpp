4a61ef97b0915f178dae3bde4effed09161265f9
Ville Sirviö <VSirvio@users.noreply.github.com>
2025-06-09 16:13:35 +0300

    Handle SIGINT and SIGTERM in log_output/main.cpp

--- log_output/main.cpp
+++ log_output/main.cpp
@@ -1,16 +1,25 @@
 // Requires C++20 or newer (e.g. GCC G++ version >= 13 with "-std=c++20" switch)
 
 #include <chrono>
+#include <csignal>
 #include <format>
 #include <iostream>
 #include <random>
 #include <string>
 #include <thread>
 
+void signalHandler(int signal)
+{
+    std::exit(EXIT_SUCCESS);
+}
+
 int main()
 {
     const std::string CHARSET = "0123456789abcdef";
     const int LENGTH = 32;
+
+    std::signal(SIGINT, signalHandler);
+    std::signal(SIGTERM, signalHandler);
 
     auto now = std::chrono::system_clock::now();
     std::chrono::nanoseconds nanosecondsSinceEpoch = now.time_since_epoch();
