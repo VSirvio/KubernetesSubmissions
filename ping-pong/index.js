8788bf75242a432f3cc82c08c0492fbf015736da
Ville Sirviö <VSirvio@users.noreply.github.com>
2025-10-09 16:23:32 +0300

    Modify ping-pong to also write pong number to file

--- ping-pong/index.js
+++ ping-pong/index.js
@@ -1,14 +1,27 @@
 import express from 'express'
+import fs from 'fs/promises'
 
 process.on('SIGINT', () => process.exit())
 process.on('SIGTERM', () => process.exit())
 
 let pongNumber = 0
 
+const writePongNumberToFile = async () => {
+  try {
+    await fs.writeFile('/usr/src/app/pongs-dir/pongs', pongNumber.toString(), 'utf8')
+  } catch (err) {
+    console.error(`File I/O failed: ${err}`)
+  }
+}
+
+await writePongNumberToFile()
+
 const app = express()
 
-app.get('/pingpong', (req, res) => {
-  res.send(`pong ${pongNumber++}`)
+app.get('/pingpong', async (req, res) => {
+  res.send(`pong ${pongNumber}`)
+  await writePongNumberToFile()
+  pongNumber++
 })
 
 app.listen(3000, () => {
