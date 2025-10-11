d200fa1dfb0147d2a081f10f6aa99e2c96ac5342
Ville Sirviö <VSirvio@users.noreply.github.com>
2025-10-11 11:03:26 +0300

    Implement hourly images in todo-app

--- the_project/todo-app/index.js
+++ the_project/todo-app/index.js
@@ -1,14 +1,38 @@
 import express from 'express'
+import fs from 'fs'
+import fsp from 'fs/promises'
+import { Readable } from 'stream'
+import { finished } from 'stream/promises'
 
 const PORT = process.env.PORT || '3000'
 
 process.on('SIGINT', () => process.exit())
 process.on('SIGTERM', () => process.exit())
 
+const downloadFile = async (from, to) => {
+  const response = await fetch(from)
+  const writeStream = fs.createWriteStream(to)
+  await finished(Readable.fromWeb(response.body).pipe(writeStream))
+}
+
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world!</h1>')
+app.use(express.static('static'))
+
+app.get('/', async (req, res) => {
+  let fileStat = null
+  let errored = false
+  try {
+    fileStat = await fsp.stat('static/image.jpg')
+  } catch {
+    errored = true
+  }
+
+  if (errored || Date.now() - fileStat.mtime > 600000) {
+    await downloadFile('https://picsum.photos/256', 'static/image.jpg')
+  }
+
+  res.send('<h1>The project App</h1><img src="image.jpg"><br><br><div>DevOps with Kubernetes 2025</div')
 })
 
 app.listen(PORT, () => {
