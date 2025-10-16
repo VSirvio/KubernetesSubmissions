1b141a1bd741947f595b3d594ebbaee5cf786a34
Ville Sirviö <VSirvio@users.noreply.github.com>
2025-10-16 17:51:44 +0300

    Modify todo-backend to use database

--- the_project/todo-backend/index.js
+++ the_project/todo-backend/index.js
@@ -1,25 +1,36 @@
 import express from 'express'
+import { DataTypes, Model, Sequelize } from 'sequelize'
 
 const PORT = process.env.PORT
 const TODOS_PATH = process.env.TODOS_PATH
+const DATABASE_URL = process.env.DATABASE_URL
 
 process.on('SIGINT', () => process.exit())
 process.on('SIGTERM', () => process.exit())
 
-const todos = []
+const sequelize = new Sequelize(DATABASE_URL, { logging: false })
+
+class Todo extends Model {}
+Todo.init(
+  { task: DataTypes.STRING },
+  { sequelize, modelName: 'Todo' }
+)
+
+await sequelize.sync()
 
 const app = express()
 
 app.use(express.json())
 
-app.get(TODOS_PATH, (req, res) => {
+app.get(TODOS_PATH, async (req, res) => {
+  const todos = await Todo.findAll()
   res.json(todos)
 })
 
-app.post(TODOS_PATH, (req, res) => {
+app.post(TODOS_PATH, async (req, res) => {
   const newTodo = req.body
-  todos.push(newTodo)
-  res.status(201).json(newTodo)
+  const createdTodo = await Todo.create(newTodo)
+  res.status(201).json(createdTodo)
 })
 
 app.listen(PORT, () => {
