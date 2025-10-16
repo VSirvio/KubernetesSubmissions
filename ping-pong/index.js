200264d2a58bf8b7a8024b3a45f0be63b2bd26d9
Ville Sirviö <VSirvio@users.noreply.github.com>
2025-10-16 16:53:56 +0300

    Modify ping-pong to use database

--- ping-pong/index.js
+++ ping-pong/index.js
@@ -1,21 +1,40 @@
 import express from 'express'
+import { DataTypes, Model, Sequelize } from 'sequelize'
 
 const PORT = process.env.PORT
+const DATABASE_URL = process.env.DATABASE_URL
 
 process.on('SIGINT', () => process.exit())
 process.on('SIGTERM', () => process.exit())
 
-let pongNumber = 0
+const sequelize = new Sequelize(DATABASE_URL, { logging: false })
+
+class PongNumber extends Model {}
+PongNumber.init(
+  { pongNumber: DataTypes.INTEGER },
+  { sequelize, modelName: 'PongNumber' },
+)
+
+await sequelize.sync()
+
+const fetchPongNumber = async () => {
+  let databaseEntries = await PongNumber.findAll()
+  if (databaseEntries.length === 0) {
+    databaseEntries = [await PongNumber.create({ pongNumber: 0 })]
+  }
+  return databaseEntries[0].pongNumber
+}
 
 const app = express()
 
-app.get('/pingpong', (req, res) => {
+app.get('/pingpong', async (req, res) => {
+  const pongNumber = await fetchPongNumber()
+  await PongNumber.update({ pongNumber: pongNumber + 1 }, { where: {} })
   res.send(`pong ${pongNumber}`)
-  pongNumber++
 })
 
-app.get('/pings', (req, res) => {
-  res.send(pongNumber)
+app.get('/pings', async (req, res) => {
+  res.send(await fetchPongNumber())
 })
 
 app.listen(PORT, () => {
