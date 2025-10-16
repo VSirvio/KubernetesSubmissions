dd1f7b3bb357fde3612ab888653b4d6055a30d90
Ville Sirviö <VSirvio@users.noreply.github.com>
2025-10-16 09:49:35 +0300

    Configure todo-app with environment variables

--- the_project/todo-app/index.js
+++ the_project/todo-app/index.js
@@ -4,7 +4,12 @@ import fsp from 'fs/promises'
 import { Readable } from 'stream'
 import { finished } from 'stream/promises'
 
-const PORT = process.env.PORT || '3000'
+const PORT = process.env.PORT
+const FRONTEND_ROOT_DIR = process.env.FRONTEND_ROOT_DIR
+const RANDOM_PICTURE_URL = process.env.RANDOM_PICTURE_URL
+const PICTURE_SAVE_DIR = process.env.PICTURE_SAVE_DIR
+const PICTURE_FILE_NAME = process.env.PICTURE_FILE_NAME
+const PICTURE_CHANGE_TIME_IN_MINUTES = process.env.PICTURE_CHANGE_TIME_IN_MINUTES
 
 process.on('SIGINT', () => process.exit())
 process.on('SIGTERM', () => process.exit())
@@ -17,25 +22,25 @@ const downloadFile = async (from, to) => {
 
 const app = express()
 
-app.use(express.static('public'))
+app.use(express.static(FRONTEND_ROOT_DIR))
 
 app.use(async (req, res, next) => {
   let fileStat = null
   let errored = false
   try {
-    fileStat = await fsp.stat('static/image.jpg')
+    fileStat = await fsp.stat(`${PICTURE_SAVE_DIR}/${PICTURE_FILE_NAME}`)
   } catch {
     errored = true
   }
 
-  if (errored || Date.now() - fileStat.mtime > 600000) {
-    await downloadFile('https://picsum.photos/256', 'static/image.jpg')
+  if (errored || Date.now() - fileStat.mtime > PICTURE_CHANGE_TIME_IN_MINUTES * 60000) {
+    await downloadFile(RANDOM_PICTURE_URL, `${PICTURE_SAVE_DIR}/${PICTURE_FILE_NAME}`)
   }
 
   next()
 })
 
-app.use(express.static('static'))
+app.use(express.static(PICTURE_SAVE_DIR))
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
